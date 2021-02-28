2828d7ecda8eb4c02332cd582fc8d83518629056
WishInLife <wishinlife@qq.com>
2021-02-28 21:15:26 +0800

    取消EasyPopupChild接口
    取消EasyPopupChild接口

--- lib/src/easy_popup_route.dart
+++ lib/src/easy_popup_route.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-import 'easy_popup_child.dart';
-
-class EasyPopupRoute extends PopupRoute {
-  final EasyPopupChild child;
+class EasyPopupRoute<T> extends PopupRoute<T> {
+  final Widget child;
   final Offset offsetLT, offsetRB;
   final Duration duration;
   final bool cancelable;
   final bool outsideTouchCancelable;
-  final bool darkEnable;
+  // final bool darkEnable;
+  final double opacity;
+  final VoidCallback onPopupShow;
+  final VoidCallback onPopupDismiss;
   final List<RRect> highlights;
 
   EasyPopupRoute({
@@ -17,10 +18,16 @@ class EasyPopupRoute extends PopupRoute {
     this.offsetRB,
     this.cancelable = true,
     this.outsideTouchCancelable = true,
-    this.darkEnable = true,
+    // this.darkEnable = true,
+    this.opacity = 0.5,
     this.duration = const Duration(milliseconds: 300),
+    this.onPopupShow,
+    this.onPopupDismiss,
     this.highlights,
-  });
+  }) {
+    assert(opacity != null && opacity <= 1.0 && opacity >= 0.0);
+    assert(duration != null);
+  }
 
   @override
   Color get barrierColor => null;
@@ -31,17 +38,34 @@ class EasyPopupRoute extends PopupRoute {
   @override
   String get barrierLabel => null;
 
+  GlobalKey<__PopRouteWidgetState> _globalKey = GlobalKey();
+  @override
+  bool didPop(T result) {
+    onPopupDismiss?.call();
+    _globalKey.currentState.dismiss();
+    return super.didPop(result);
+  }
+
+  @override
+  TickerFuture didPush() {
+    onPopupShow?.call();
+    return super.didPush();
+  }
+
   @override
   Widget buildPage(BuildContext context, Animation<double> animation,
       Animation<double> secondaryAnimation) {
     return _PopRouteWidget(
+      key: _globalKey,
       child: child,
       offsetLT: offsetLT,
       offsetRB: offsetRB,
       duration: duration,
+      // animation: animation,
       cancelable: cancelable,
       outsideTouchCancelable: outsideTouchCancelable,
-      darkEnable: darkEnable,
+      // darkEnable: darkEnable,
+      opacity: opacity,
       highlights: highlights,
     );
   }
@@ -49,8 +73,17 @@ class EasyPopupRoute extends PopupRoute {
   @override
   Duration get transitionDuration => duration;
 
+  // @override
+  // Animation<double> createAnimation() {
+  //   return CurvedAnimation(
+  //       parent: super.createAnimation(),
+  //       curve: Curves.linear//,
+  //       // reverseCurve: Curves.linear
+  //   );// const Interval(0.0, 2.0/3.0));
+  // }
+
   static pop(BuildContext context) {
-    __PopRouteWidgetState.of(context).dismiss();
+    // __PopRouteWidgetState.of(context).dismiss();
     Navigator.of(context).pop();
   }
 
@@ -60,45 +93,53 @@ class EasyPopupRoute extends PopupRoute {
 }
 
 class _PopRouteWidget extends StatefulWidget {
-  final EasyPopupChild child;
+  final Key key;
+  final Widget child;
   final Offset offsetLT, offsetRB;
   final Duration duration;
+  // final Animation<double> animation;
   final bool cancelable;
   final bool outsideTouchCancelable;
-  final bool darkEnable;
+  // final bool darkEnable;
+  final double opacity;
   final List<RRect> highlights;
 
+
   _PopRouteWidget({
+    this.key,
     this.child,
     this.offsetLT,
     this.offsetRB,
     this.duration,
+    // this.animation,
     this.cancelable,
     this.outsideTouchCancelable,
-    this.darkEnable,
+    // this.darkEnable,
+    this.opacity,
     this.highlights,
-  });
+  }):super(key: key);
 
   @override
   __PopRouteWidgetState createState() => __PopRouteWidgetState();
 }
 
 class __PopRouteWidgetState extends State<_PopRouteWidget>
-    with SingleTickerProviderStateMixin {
-  Animation<double> alphaAnim;
-  AnimationController alphaController;
+    with SingleTickerProviderStateMixin
+{
+  Animation<double> _alphaAnim;
+  AnimationController _alphaController;
   List<RRect> _highlights = [];
 
   @override
   void initState() {
     super.initState();
     highlights = widget.highlights ?? [];
-    alphaController = AnimationController(
+    _alphaController = AnimationController(
       vsync: this,
       duration: widget.duration,
     );
-    alphaAnim = Tween<double>(begin: 0, end: 127).animate(alphaController);
-    alphaController.forward();
+    _alphaAnim = Tween<double>(begin: 0, end: 255).animate(_alphaController);
+    _alphaController.forward();
   }
 
   static __PopRouteWidgetState of(BuildContext context) {
@@ -106,8 +147,7 @@ class __PopRouteWidgetState extends State<_PopRouteWidget>
   }
 
   dismiss() {
-    alphaController?.reverse();
-    widget.child?.dismiss();
+    _alphaController?.reverse();
   }
 
   set highlights(List<RRect> value) {
@@ -118,7 +158,7 @@ class __PopRouteWidgetState extends State<_PopRouteWidget>
 
   @override
   void dispose() {
-    alphaController?.dispose();
+    _alphaController?.dispose();
     super.dispose();
   }
 
@@ -127,7 +167,7 @@ class __PopRouteWidgetState extends State<_PopRouteWidget>
     return WillPopScope(
       onWillPop: () async {
         if (widget.cancelable) {
-          dismiss();
+          // dismiss();
           return true;
         }
         return false;
@@ -135,15 +175,15 @@ class __PopRouteWidgetState extends State<_PopRouteWidget>
       child: GestureDetector(
         onTap: () {
           if (widget.outsideTouchCancelable) {
-            dismiss();
+            // dismiss();
             Navigator.of(context).pop();
           }
         },
         child: Stack(
           children: <Widget>[
-            widget.darkEnable
+            widget.opacity != 0
                 ? AnimatedBuilder(
-                    animation: alphaController,
+                    animation: _alphaController , //widget.animation,
                     builder: (_, __) {
                       return Padding(
                         padding: EdgeInsets.only(
@@ -154,7 +194,7 @@ class __PopRouteWidgetState extends State<_PopRouteWidget>
                         ),
                         child: ColorFiltered(
                           colorFilter: ColorFilter.mode(
-                            Colors.black.withAlpha(alphaAnim.value.toInt()),
+                            Colors.black.withAlpha((_alphaAnim.value * widget.opacity).toInt()),
                             BlendMode.srcOut,
                           ),
                           child: Stack(
